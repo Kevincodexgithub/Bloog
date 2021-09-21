26ec6790ac0794389b2e8552d0660e0644c8aa65
Kevincodexgithub <kevintauro95@gmail.com>
2021-09-21 18:44:10 -0500

    filtrar posts por categorias

---
+++ resources/views/posts/category.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
+        <h1 class="uppercase text-3xl font-bold text-center">Categoria: {{ $category->name }}</h1>
+
+        @foreach ($posts as $post)
+            <article class="mb-8 bg-white shadow-md rounded-lg overflow-hidden hover:shadow-2xl">
+                <img class="w-full h-72 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">
+                <div class="px-6 py-4">
+                    <h1 class="font-bold text-xl mb-2">
+                        <a href="{{ route('posts.show', $post) }}">{{ $post->name }}</a>
+                    </h1>
+                    <div class="text-gray-700 text-base">
+                        {{ $post->extract }}
+                    </div>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    @foreach ($post->tags as $tag)
+                        <a href="#" class="inline-block bg-gray-200 rounded-full py-1 px-3 text-sm text-gray-700 mr-2">
+                            {{ $tag->name }}
+                        </a>
+                    @endforeach
+                </div>
+            </article>
+        @endforeach
+
+        <div class="mt-4">
+            {{ $posts->links() }}
+        </div>
+    </div>
+
+
+</x-app-layout>
